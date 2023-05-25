b27b7dc2105713cd303f967bde30ebc799d4f9b6
curtis <curtis@serverascode.com>
2023-05-25 09:44:34 -0400

    update to use new config.json config

--- whydah/routes.py
+++ whydah/routes.py
@@ -15,14 +15,28 @@ def config_bp(config_manager):
             abort(404, f"Config for service '{service_name}' not found")
         return jsonify(config)
 
-    @bp.route("/config/<service_name>/<setting_name>", methods=["POST"])
-    def update_config(service_name, setting_name):
+    @bp.route("/config/<service_name>/<setting_name>/<property_name>", methods=["POST"])
+    def update_config_property(service_name, setting_name, property_name):
         """
-        Update a configuration setting for a service
+        Update a specific property of a configuration setting for a service
         """
+        # if service name or setting name is not in cache
+        if service_name not in config_manager.config_cache:
+            abort(404, f"Config for service '{service_name}' not found")
+
+        if setting_name not in config_manager.config_cache[service_name]:
+            abort(
+                404, f"Setting '{setting_name}' not found for service '{service_name}'"
+            )
+
+        # get the new value
         new_value = request.json.get("value")
-        if new_value is None:
-            abort(400, "Missing 'value' in the request body")
+        if not new_value:
+            abort(400, "Missing value in request body")
+
+        # check if property_name is valid
+        if property_name not in ["value", "type", "enabled"]:
+            abort(400, f"Invalid property name '{property_name}'")
 
         # Validate the new_value
         if not isinstance(new_value, str) or new_value.strip() == "":
@@ -31,12 +45,20 @@ def config_bp(config_manager):
         if len(new_value) > 1024:
             abort(400, f"Maximum length of value is 1024 characters")
 
-        success = config_manager.update_config(service_name, setting_name, new_value)
+        success = config_manager.update_config(
+            service_name, setting_name, property_name, new_value
+        )
         if success:
-            return jsonify({"status": f"Updated '{setting_name}' for '{service_name}'"})
+            return jsonify(
+                {
+                    "status": f"Updated '{property_name}' of '{setting_name}' for '{service_name}'"
+                }
+            )
         else:
-            abort(404, f"Failed to update '{setting_name}' for '{service_name}'")
-
+            abort(
+                404,
+                f"Failed to update '{property_name}' of '{setting_name}' for '{service_name}'",
+            )
 
     @bp.route("/config/refresh", methods=["POST"])
     def refresh_configs():
